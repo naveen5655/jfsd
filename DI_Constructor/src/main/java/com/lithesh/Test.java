fd419f71278bb3976317376a1aa444588b2bf9cd
Badisa Naveen <89301294+naveen5655@users.noreply.github.com>
2022-09-21 03:41:27 +0530

    Add files via upload

---
+++ DI_Constructor/src/main/java/com/lithesh/Test.java
@@ -0,0 +1,17 @@
+package com.lithesh;
+
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class Test {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("beanContext.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("employee");  
+        s.show();  
+          
+    }  
+}  
