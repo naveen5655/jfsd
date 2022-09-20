05dbd00991be026504ff168fcdcb5171a8c8d70a
Badisa Naveen <89301294+naveen5655@users.noreply.github.com>
2022-09-21 03:47:10 +0530

    Add files via upload

---
+++ SpringConstructor/src/main/java/naveen/Test.java
@@ -0,0 +1,16 @@
+package naveen;
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class Test {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("beanContext.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("employee");  
+        s.show();  
+          
+    }  
+}  
